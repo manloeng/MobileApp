53a20b481cf97bbfa2f82a9ab1611c4d9e4c5abc
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-02 21:25:27 +0100

    Remove unused ScrollView

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import Movies from './src/movies';
 
 const App = () => {
   return (
     <SafeAreaView>
       <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic" />
       <Movies />
     </SafeAreaView>
   );
