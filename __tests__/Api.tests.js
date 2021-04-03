a57a277694152e68b674d949ca1dd7395b92b1a5
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-03 22:49:59 +0100

    Update Api test

--- __tests__/Api.tests.js
+++ __tests__/Api.tests.js
@@ -2,7 +2,7 @@ const {getMoviesByPopulatrity, getMoviesViaInput} = require('../client');
 
 describe('API Testsing', () => {
   describe('getMoviesByPopulatrity', () => {
-    it('Returns the first page of popular movies', async () => {
+    it('Returns a 200 Success: returns the first page of popular movies', async () => {
       const response = await getMoviesByPopulatrity();
       expect(response).toBeTruthy();
       expect(response.status).toEqual(200);
@@ -10,15 +10,21 @@ describe('API Testsing', () => {
       expect(response.data.page).toEqual(1);
     });
 
-    it('Return the second page popular movies', async () => {
+    it('Return a 200 Sucess: returns the second page popular movies', async () => {
       const response = await getMoviesByPopulatrity(2);
       expect(response.status).toEqual(200);
       expect(response.data.page).toEqual(2);
     });
+
+    it('Return a 400 Bad Request: when a invalid page number is entered', async () => {
+      const response = await getMoviesByPopulatrity('Fail');
+      expect(response.status).toEqual(400);
+      expect(response.msg).toEqual('Please enter a valid page number');
+    });
   });
 
   describe('getMoviesViaInput', () => {
-    it('Returns Just one movie when no input is passed', async () => {
+    it('Returns a 200 Success: Returns Just one movie when no input is passed', async () => {
       const response = await getMoviesViaInput();
       expect(response).toBeTruthy();
       expect(response.status).toEqual(200);
@@ -26,13 +32,19 @@ describe('API Testsing', () => {
       expect(response.data.page).toEqual(1);
     });
 
-    it('Return the second page popular movies', async () => {
+    it('Returns a 200 Success: Return movies containing Marvel', async () => {
       const response = await getMoviesViaInput('Marvel');
       expect(response).toBeTruthy();
       expect(response.status).toEqual(200);
       expect(response.data.results.length).toEqual(20);
       expect(response.data.page).toEqual(1);
       expect(response.data.results[0].title).toContain('Marvel');
+    });
+
+    it('Returns a 200 Success: Return a movie list with no movies when passed with gibberish', async () => {
+      const response = await getMoviesViaInput('!@£$%');
+      expect(response).toBeTruthy();
+      expect(response.data.results.length).toEqual(0);
     });
   });
 });
